b996e308e3b9c30e43cca178a0838e7139cf84cd
altafraza14307-blip <altafraza14307@gmail.com>
2025-09-07 13:46:02 +0530

    error correction

--- personal_diary.py
+++ personal_diary.py
@@ -1,9 +1,9 @@
-def odd_num_addition(list):
+def odd_num_addition(arr):
     add = 0
-    for i in list:
+    for i in arr:
         if i%2!=0:
-            add+=1
-        return add
+            add+=i
+    return add
 list = [12,3,7,8]
 res = odd_num_addition(list)  
 print(res)
